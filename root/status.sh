5462f0a274a288a108a2d8cc3a1835cf30877814
LamaBleu <lama.bleu@gmail.com>
2019-05-29 18:31:15 +0200

    Get TX settings

--- root/status.sh
+++ root/status.sh
@@ -2,12 +2,20 @@ cd /sys/bus/iio/devices/iio\:device1
 while :
 do
 	reset
+	echo "     ****** RX ******"
 	echo "Freq       : " $(cat out_altvoltage0_RX_LO_frequency)
 	echo "Samplerate : " $(cat in_voltage_sampling_frequency)
 	echo "BW         : " $(cat in_voltage_rf_bandwidth)
 	echo "Gain       : " $(cat in_voltage0_hardwaregain) " ("$(cat in_voltage0_gain_control_mode)" mode)"
-	echo "Level      : " $(cat in_voltage0_rssi)
-	sleep 3
+	echo "RSSI       : " $(cat in_voltage0_rssi)
+	echo
+	echo 
+	echo "     ****** TX ******"
+        echo "Freq       : " $(cat out_altvoltage1_TX_LO_frequency)
+        echo "Samplerate : " $(cat out_voltage_sampling_frequency)
+        echo "BW         : " $(cat out_voltage_rf_bandwidth)
+        echo "Att.       : " $(cat out_voltage0_hardwaregain)
+sleep 3
 
 done
 
